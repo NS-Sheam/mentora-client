00613d7f5ba750f0679cd8769582174fae6f3440
NS-Sheam <123sheamfeni@gmail.com>
2026-05-15 20:09:25 +0600

    add:starter pack

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,8 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      class="light"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased light`}
       data-theme="light"
       suppressHydrationWarning
     >
